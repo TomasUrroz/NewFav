0c6c5eb553bf1a0c279c50cdf932593c800222a3
Thomo <tomasurroz@gmail.com>
2023-11-02 14:58:29 -0300

    Enrutamiento basico, creacion de componentes y organizacion de carpetas.

--- newFav/src/app/app-routing.module.ts
+++ newFav/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { ProfilePageComponent } from './Pages/profile-page/profile-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomePageComponent},
+  {path: "profile", component: ProfilePageComponent},
+  {path: "**", redirectTo:"home"},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- newFav/src/app/app.module.ts
+++ newFav/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { NavBarComponent } from './Components/Shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './Components/Shared/footer/footer.component';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { ProfilePageComponent } from './Pages/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomePageComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
