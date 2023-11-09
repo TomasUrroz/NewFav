b3787825d7062b0770946130a5bb3748061b19c0
Tomas Urroz <tomasurroz@gmail.com>
2023-11-08 22:03:24 -0300

    Se ven los datos de una persona

--- newFav/src/app/Components/home/home.component.ts
+++ newFav/src/app/Components/home/home.component.ts
@@ -1,50 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { redirectToAuthCodeFlow, getAccessToken} from "./authCodeWithPkce";
+import { AuthService } from 'src/app/Services/auth.service';
+import { UtilsUserService } from 'src/app/Services/utils-user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit{
-
-  clientId:string = "b20bc978b3e240b997fbd6734c1d405d"; // Replace with your client id
-  params:any = new URLSearchParams(window.location.search);
-  code:any = this.params.get("code");
+export class HomeComponent implements OnInit {
   
-  ngOnInit(): void {
-    this.needed()
+  clientId: string = 'b20bc978b3e240b997fbd6734c1d405d';
+  params: any = new URLSearchParams(window.location.search);
+  code: any = this.params.get('code');
 
+  constructor(private as: AuthService, private uss: UtilsUserService) {}
+
+  ngOnInit(): void {
+    console.log("XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+    this.needed();
   }
 
-  async needed(){
+  async needed() {
     if (!this.code) {
-      redirectToAuthCodeFlow(this.clientId);
+      this.as.redirectToAuthCodeFlow(this.clientId);
     } else {
-        const accessToken = await getAccessToken(this.clientId, this.code);
-        const profile = await this.fetchProfile(accessToken);
-        this.populateUI(profile);
+      const accessToken = await this.as.getAccessToken(
+        this.clientId,
+        this.code
+      );
+      const profile = await this.uss.fetchProfile(accessToken);
+      this.uss.populateUI(profile);
     }
   }
-
-  async fetchProfile(code: string): Promise<UserProfile> {
-      const result = await fetch("https://api.spotify.com/v1/me", {
-          method: "GET", headers: { Authorization: `Bearer ${code}` }
-      });
-  
-      return await result.json();
-  }
-  
-  populateUI(profile: UserProfile) {
-      document.getElementById("displayName")!.innerText = profile.display_name;
-      document.getElementById("avatar")!.setAttribute("src", profile.images[0].url)
-      document.getElementById("id")!.innerText = profile.id;
-      document.getElementById("email")!.innerText = profile.email;
-      document.getElementById("uri")!.innerText = profile.uri;
-      document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
-      document.getElementById("url")!.innerText = profile.href;
-      document.getElementById("url")!.setAttribute("href", profile.href);
-      document.getElementById("imgUrl")!.innerText = profile.images[0].url;
-  }
-
 }
