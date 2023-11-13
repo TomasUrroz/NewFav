a046e6b0048a4b3705751efeb4b8fa94bbc1f5c4
Tomas Urroz <tomasurroz@gmail.com>
2023-11-13 20:18:22 -0300

    ni idea we

---
+++ newFav/src/app/Components/Shared/display-song/display-song.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-song',
+  templateUrl: './display-song.component.html',
+  styleUrls: ['./display-song.component.css']
+})
+export class DisplaySongComponent {
+
+  constructor(){}
+
+  async getTrack(){
+    
+  }
+}
